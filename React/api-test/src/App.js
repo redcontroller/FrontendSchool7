eaacc0c45c7dfbb78e029f37cbee60a39824bca9
redcontroller <jasinis102@gmail.com>
2023-10-06 18:12:19 +0900

    20231006 api 특강

--- React/api-test/src/App.js
+++ React/api-test/src/App.js
@@ -1,7 +1,38 @@
+import LoginPage from "./Components/LoginPage";
+import JoinPage from "./Components/JoinPage";
+import { useState } from "react";
+
 function App() {
+  const [page, setPage] = useState(true);
+  const [info, setInfo] = useState();
+  const handlePage = () => {
+    setPage(prevPage => !prevPage)
+  }
+
+const getMyinfo = async () => {
+  const token = localStorage.getItem("token");
+  console.log(token);
+  const reqUrl = "https://api.mandarin.weniv.co.kr/user/myinfo";
+  const res = await fetch(reqUrl, {
+    method: "GET",
+    headers: {
+      "Authorization" : `Bearer ${token}`
+    }
+  })
+  const json = await res.json();
+  console.log(json);
+  setInfo(JSON.stringify(json));
+  // setInfo(JSON.stringify(json.user.accountname));
+}
+
+
+
+  // 로그인 기능!
   return (
     <div>
-      hello world
+      <button type="button" onClick={getMyinfo}>내 정보 불러오기</button>
+      {info}
+      {page ? <LoginPage handlePage={handlePage}/> : <JoinPage handlePage={handlePage}/>}
     </div>
   );
 }
